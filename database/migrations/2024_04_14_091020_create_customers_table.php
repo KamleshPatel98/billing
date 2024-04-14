4007e4c82eade4500a633dcad76c1f092fcd49b4
KamleshPatel98 <kamleshp52170@gmail.com>
2024-04-14 18:34:37 +0530

    change opening balance string to integer

--- database/migrations/2024_04_14_091020_create_customers_table.php
+++ database/migrations/2024_04_14_091020_create_customers_table.php
@@ -4,6 +4,7 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
+
 return new class extends Migration
 {
     /**
@@ -16,7 +17,7 @@ return new class extends Migration
             $table->string('cust_name',70);
             $table->string('mobile',10);
             $table->text('address');
-            $table->string('opening_bal');
+            $table->integer('opening_bal');
             $table->timestamps();
         });
     }
