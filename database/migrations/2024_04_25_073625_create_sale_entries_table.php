f990d4fb6bd213638ceb1ec1f38163dc90831542
KamleshPatel98 <kamleshp52170@gmail.com>
2024-05-21 11:15:26 +0530

    add upper table of sale entry

---
+++ database/migrations/2024_04_25_073625_create_sale_entries_table.php
@@ -0,0 +1,31 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('sale_entries', function (Blueprint $table) {
+            $table->id();
+            $table->date('sale_date');
+            $table->integer('bill_no');
+            $table->bigInteger('customer_id')->unsigned();
+            $table->foreign('customer_id')->references('id')->on('customers')->onDelete('cascade');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('sale_entries');
+    }
+};
