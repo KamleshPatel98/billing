1d4dee49854aa3669c4b00d008c2259180c12bf8
KamleshPatel98 <kamleshp52170@gmail.com>
2024-05-21 23:01:49 +0530

    add saleEntryDetails,sale_invoice

--- routes/web.php
+++ routes/web.php
@@ -10,6 +10,7 @@ use App\Http\Controllers\UnitController;
 use App\Http\Controllers\PartyController;
 use App\Http\Controllers\SaleEntryController;
 use App\Http\Controllers\SaleItemEntryController;
+use App\Http\Controllers\InvoiceController;
 
 Route::get('/', function () {
     return view('welcome');
@@ -33,7 +34,10 @@ Route::middleware(['auth'])->group(function () {
     Route::resource('party',PartyController::class);
     Route::resource('saleEntry',SaleEntryController::class);
     Route::get('addSaleEntry',[SaleEntryController::class,'addSaleEntry'])->name('addSaleEntry');
+    Route::get('saleEntryDetails',[SaleEntryController::class,'saleEntryDetails'])->name('saleEntry.saleEntryDetails');
     Route::resource('saleItemEntry',SaleItemEntryController::class);
     Route::get('storeSaleLowerEntry',[SaleItemEntryController::class,'storeSaleLowerEntry'])->name('storeSaleLowerEntry');
     
+    //Invoice
+    Route::get('sale_invoice',[InvoiceController::class,'sale_invoice'])->name('invoice.sale_invoice');
 });
